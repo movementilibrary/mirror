0fbed46b11988dcf9256bbab689955393f85b414
michelmarciano <michel.marciano@dasa.com.br>
2019-09-28 08:46:20 -0300

    alteracao de arquivo

--- src/main/java/br/com/dasa/mirror/api/service/impl/UnitService.java
+++ src/main/java/br/com/dasa/mirror/api/service/impl/UnitService.java
@@ -26,7 +26,7 @@ public class UnitService {
         LOGGER.info("meu diretorio atual" + path);
         try {
             Gson gson = new Gson();
-            BufferedReader json = new BufferedReader(new FileReader("src/main/resources/static/unidade.txt"));
+            BufferedReader json = new BufferedReader(new FileReader("src/main/resources/unidade.txt"));
 
             Type listaUnidadeDeserializa = new TypeToken<ArrayList<Unit>>() {
             }.getType();
