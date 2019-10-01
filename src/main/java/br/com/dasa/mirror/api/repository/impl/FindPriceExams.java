ea774f97e10938f01410182bebad41880406a937
Diego Diniz <diego.diniz@dasa.com.br>
2019-10-01 11:27:01 -0300

    [GPA-135] Merge do projeto

--- src/main/java/br/com/dasa/mirror/api/repository/impl/FindPriceExams.java
+++ src/main/java/br/com/dasa/mirror/api/repository/impl/FindPriceExams.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
-import java.util.logging.Level;
 
 @Repository
 public class FindPriceExams {
