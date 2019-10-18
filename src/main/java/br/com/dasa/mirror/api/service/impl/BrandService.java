f9acd5b11de13b435b14117af57c0f6bb30275cc
michelmarciano <michel.marciano@dasa.com.br>
2019-10-17 21:00:06 -0300

    inclusao controller

--- src/main/java/br/com/dasa/mirror/api/service/impl/BrandService.java
+++ src/main/java/br/com/dasa/mirror/api/service/impl/BrandService.java
@@ -1,6 +1,9 @@
 package br.com.dasa.mirror.api.service.impl;
 
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,12 +20,17 @@ import com.google.gson.reflect.TypeToken;
 import br.com.dasa.mirror.api.enumeration.TypeFile;
 import br.com.dasa.mirror.api.model.Brand;
 import br.com.dasa.mirror.api.model.Unit;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Service
 public class BrandService {
 	
-	@Autowired
-	private AwsS3Service awsS3Service;
+//	@Autowired
+//	private AwsS3Service awsS3Service;
+
+    @Autowired
+    ResourceLoader resourceLoader;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Brand.class);
 
@@ -32,12 +40,19 @@ public class BrandService {
         Gson gson = new Gson();
 
         try {
-        	StringBuilder json = awsS3Service.acessS3Aws(TypeFile.BRAND);
+        	//StringBuilder json = awsS3Service.acessS3Aws(TypeFile.BRAND);
+
+            final Resource resource = this.resourceLoader.getResource("classpath:marca");
+            final InputStream inputStream = resource.getInputStream();
+            File file = File.createTempFile("marca", ".tmp");
+            Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            String json = new String(Files.readAllBytes(file.toPath()));
+            inputStream.close();
 
             Type listaMarcaDeserializa = new TypeToken<ArrayList<Unit>>() {
             }.getType();
 
-            List<Unit> listaUnit = gson.fromJson(json.toString(), listaMarcaDeserializa);
+            List<Unit> listaUnit = gson.fromJson(json, listaMarcaDeserializa);
 
             idDataProvider = Optional.of(listaUnit.stream()
                     .filter(g -> idGliese.equals(g.getIdGliese()))
--- src/main/java/br/com/dasa/mirror/api/service/impl/UnitService.java
+++ src/main/java/br/com/dasa/mirror/api/service/impl/UnitService.java
@@ -1,6 +1,9 @@
 package br.com.dasa.mirror.api.service.impl;
 
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,24 +20,38 @@ import com.google.gson.reflect.TypeToken;
 import br.com.dasa.mirror.api.enumeration.TypeFile;
 import br.com.dasa.mirror.api.model.Unit;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
 @Service
 public class UnitService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Unit.class);
 
+//	@Autowired
+//	private AwsS3Service awsS3Service;
+
 	@Autowired
-	private AwsS3Service awsS3Service;
+	ResourceLoader resourceLoader;
 
 	public Integer convertUnityGlieseToUnityDataProvider(String idGliese) {
 		Optional<Integer> idDataProvider = null;
 		try {
 			Gson gson = new Gson();
-			StringBuilder json = awsS3Service.acessS3Aws(TypeFile.UNIT);
+			//StringBuilder json = awsS3Service.acessS3Aws(TypeFile.UNIT);
+
+			final Resource resource = this.resourceLoader.getResource("classpath:unidade");
+			final InputStream inputStream = resource.getInputStream();
+			File file = File.createTempFile("unidade", ".tmp");
+			Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			String json = new String(Files.readAllBytes(file.toPath()));
+
+			inputStream.close();
 
 			Type listaUnidadeDeserializa = new TypeToken<ArrayList<Unit>>() {
 			}.getType();
 
-			List<Unit> listaUnit = gson.fromJson(json.toString(), listaUnidadeDeserializa);
+			List<Unit> listaUnit = gson.fromJson(json, listaUnidadeDeserializa);
 
 			idDataProvider = Optional.of(listaUnit.stream().filter(g -> idGliese.equals(g.getIdGliese()))
 					.map(Unit::getIdDataProvider).findAny()
